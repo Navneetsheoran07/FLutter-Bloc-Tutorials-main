1c21f90d08ac34c6f18e42d16c33731f0f45b4e8
Navneet sheoran <navneetsheoran537@gmail.com>
2024-02-05 17:52:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:bloc_tutorials/Utils/image_picker_utils.dart';
+import 'package:bloc_tutorials/bloc/counter_bloc/counter_bloc.dart';
+import 'package:bloc_tutorials/bloc/favourite/favourite_bloc.dart';
+import 'package:bloc_tutorials/bloc/favourite/favourite_event.dart';
+import 'package:bloc_tutorials/bloc/form_validation_bloc/my_form_bloc.dart';
+import 'package:bloc_tutorials/bloc/image_picker/image_picker_bloc.dart';
+import 'package:bloc_tutorials/bloc/switch_bloc/switch_bloc.dart';
+import 'package:bloc_tutorials/repository/favourite_reposiotry.dart';
+import 'package:bloc_tutorials/view/counter/counter_screen.dart';
+import 'package:bloc_tutorials/view/favourite/favourite_screen.dart';
+
+import 'package:bloc_tutorials/view/todo/post_screen.dart';
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/todo/to_do_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => CounterBloc()),
+          BlocProvider(create: (_) => MyFormBloc()),
+          BlocProvider(create: (_) => SwitchBloc()),
+          BlocProvider(create: (_) => ToDoBloc()),
+          BlocProvider(create: (_) => ImagePickerBloc(ImagePickerUtils())),
+          BlocProvider(create: (_) => FavouriteBloc(FavouriteRepository())..add(FetchFavouriteList())),
+
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home:  PostScreen(),
+        ));
+  }
+}
---
+++ lib/view/counter/home_screen.dart
@@ -0,0 +1,82 @@
+
+import 'package:bloc_tutorials/view/counter/counter_screen.dart';
+import 'package:bloc_tutorials/view/todo/post_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../form_valiation/form_sreen.dart';
+
+import '../switch_button/switch_widget.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Block Tutorials'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: ListView(
+            children: [
+              Card(
+                child: ListTile(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const CounterScreen()));
+                  },
+                  title: const Text('Counter Example'),
+                  subtitle: const Text('Simple example to increment or decrement the counter'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SwitchWidget()));
+                  },
+                  title: const Text('Some Bloc Example'),
+                  subtitle: const Text('Switch button, counter exmaple and slider example with container'),
+                ),
+              ),
+
+              Card(
+                child: ListTile(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const MyForm()));
+                  },
+                  title: const Text('Login Form Validation'),
+                  subtitle: const Text('Simple login form to validating using bloc '),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const PostScreen()));
+                  },
+                  title: const Text('Get Post Api with bloc'),
+                  subtitle: const Text('We will fetch the list of post using bloc'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => PostScreen()));
+                  },
+                  title: const Text('Post'),
+                  subtitle: const Text('Post with lazy loading using bloc....'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
